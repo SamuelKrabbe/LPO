0e4c74735518eedb03e519c95a4c11d8a50abd9f
Samuel Krabbe <101955583+SamuelKrabbe@users.noreply.github.com>
2023-04-10 08:54:03 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,26 +1,93 @@
 import java.util.Scanner;
-import matrizes.Matriz;
+
+class Veiculo{
+    private String cor;
+    private int numPassageiros;
+
+    //construtor
+    Veiculo(String cor, int numPassageiros){
+        this.cor = cor;
+        this.numPassageiros = numPassageiros;
+    }
+
+    //métodos
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+
+    public void setNumPassageiros(int numPassageiros){
+        this.numPassageiros = numPassageiros;
+    }
+
+    public String getCor(){
+        return this.cor;
+    }
+
+    public int getNumPassageiros(){
+        return this.numPassageiros;
+    }
+
+    @Override
+    public String toString(){
+        return this.cor + " numPassageiros " + this.numPassageiros;
+    }   
+}
+
+class Carro extends Veiculo{
+    private String marca;
+    private String modelo;
+
+    //construtor
+    Carro(String cor, int numPassageiros, String marca, String modelo){
+        super(cor, numPassageiros);
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    //métodos
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public String getMarca(){
+        return this.marca;
+    }
+
+    public String getModelo(){
+        return this.modelo;
+    }
+    
+    public void acelerar(){
+        System.out.println(String.format("O %s acelerou", modelo));
+    }
+
+    public void freiar(){
+        System.out.println(String.format("O %s freou", modelo));
+    }
+
+    @Override
+    public String toString(){
+        return "Carro " + this.marca + " " + this.modelo + " " + super.getCor() + " numPassageiros " + super.getNumPassageiros();
+    }   
+}
 
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int linha = sc.nextInt();
-        int coluna = sc.nextInt();
-        int soma;
-        Matriz A = new Matriz(linha, coluna);
-        Matriz B = new Matriz(linha, coluna);
-        Matriz C = new Matriz(linha, coluna);;
+        String cor = sc.next();
+        int numPassageiros = sc.nextInt();
+        String marca = sc.next();
+        String modelo = sc.next();
+        Carro carro = new Carro(cor, numPassageiros, marca, modelo);
 
-        A.lerMatriz("A");
-        B.lerMatriz("B");
-        A.imprimirMatriz("A");
-        B.imprimirMatriz("B");
-        A.procurarElemento(2);
-        soma = A.somarElementosMatriz("A");
-        C.setMat(Matriz.somarMatrizes(A.getMat(), B.getMat()));
-        C.setMat(Matriz.subtrairMatrizes(A.getMat(), B.getMat()));
-
-        System.out.println(soma);
-        sc.close();
+        System.out.println(carro);
+        carro.setModelo(sc.next());
+        System.out.println(carro);
+        carro.acelerar();
+        carro.freiar();
     }
 }
